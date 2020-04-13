31d40a62ce7e0a816db5f1c250c6526a382d52ba
deetrive <deetrive@WKWIN6029839.global.publicisgroupe.net>
2020-04-13 23:54:34 +0530

    Observer Design Pattern

---
+++ java-design/src/com/java/behavioral/Observer/DataPresenter.java
@@ -0,0 +1,5 @@
+package com.java.behavioral.Observer;
+
+public interface DataPresenter {
+	public void showData();
+}
---
+++ java-design/src/com/java/behavioral/Observer/Observer.java
@@ -0,0 +1,5 @@
+package com.java.behavioral.Observer;
+
+public interface Observer {
+	public void update(int pressure, int temperature, int humidity);
+}
